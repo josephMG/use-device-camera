1bb3b66b7f9a6bac31f41bfd7a2de7311d6363ab
JosephMG <joseph780116.mg@gmail.com>
2025-12-28 17:04:13 +0800

    rewrite package.json

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,12 @@ export default defineConfig({
     vanillaExtractPlugin(),
     react()
   ],
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    }
+  },
   server: {
     // allowedHosts: ['localhost']
   }

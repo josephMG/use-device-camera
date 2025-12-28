d5d68e2005573f38199c91e93e06ab37c7aaa57c
JosephMG <joseph780116.mg@gmail.com>
2025-12-28 16:42:52 +0800

    add gh-page

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/use-device-camera/',
   plugins: [
     vanillaExtractPlugin(),
     react()
   ],
   server: {
-    allowedHosts: ['localhost']
+    // allowedHosts: ['localhost']
   }
 })
